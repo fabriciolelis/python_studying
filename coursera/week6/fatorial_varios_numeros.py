273e65373328feb8acdc4f9477a0a0066f181b91
Fabricio Lelis <fabriciolelis@gmail.com>
2017-02-13 22:27:01 -0300

    started week 6

---
+++ coursera/week6/fatorial_varios_numeros.py
@@ -0,0 +1,8 @@
+n = int(input("Digite um número inteiro positivo: "))
+while n > 0:
+    fatorial = 1
+    while n > 1:
+        fatorial = fatorial * n
+        n = n - 1
+    print(fatorial)
+    n = int(input("Digite um novo número inteiro positivo: "))
