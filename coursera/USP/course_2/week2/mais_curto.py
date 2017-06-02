6e1472a737c4809d3f3cc551f8dfb88ef343652a
Fabricio Lelis <fabriciolelis@gmail.com>
2017-06-01 21:10:28 -0300

    Professor Challenge

---
+++ coursera/USP/course_2/week2/mais_curto.py
@@ -0,0 +1,12 @@
+def mais_curto(lista_nomes):
+    nome_mais_curto = lista_nomes[0].strip()
+    for nome in lista_nomes:
+        if(len(nome_mais_curto) > len(nome.strip())):
+            nome_mais_curto = nome.strip()
+
+    return nome_mais_curto.capitalize()
+
+lista = ["larissa", "joão", "fabricio", "    Ju", "Solange    ", "Julianne"]
+
+print(mais_curto(lista))
+
