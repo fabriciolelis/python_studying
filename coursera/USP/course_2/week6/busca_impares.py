4a6199fd9bd4f08bec0e85cfebe6ca38592e8e4d
Fabrício Lélis <fabriciolelis@gmail.com>
2017-07-18 22:57:29 -0300

    Finish the course 2

--- coursera/USP/course_2/week6/busca_impares.py
+++ coursera/USP/course_2/week6/busca_impares.py
@@ -1,4 +1,4 @@
-def busca_impares(lista):
+def encontra_impares(lista):
     tamanho_lista = len(lista)
     if tamanho_lista == 1 and lista[0] % 2 != 0:
         nlista = [lista[0]]
@@ -7,12 +7,9 @@ def busca_impares(lista):
         return []
     elif lista[len(lista) - 1] % 2 != 0:
         alista = [lista[len(lista) - 1]]
-        alista.extend(busca_impares(lista[0:len(lista) - 1]))
+        alista.extend(encontra_impares(lista[0:len(lista) - 1]))
         return alista
     else:
         alista = []
-        alista.extend(busca_impares(lista[0:len(lista) - 1]))
+        alista.extend(encontra_impares(lista[0:len(lista) - 1]))
         return alista
-
-
-print(busca_impares([5, 3, 8, 9]))
