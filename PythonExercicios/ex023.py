10c2931356e060be4c38432f2ef481057c1b6479
Fabricio Melo <fabricio.melo@virtus.ufcg.edu.br>
2018-02-06 10:56:05 -0300

    terminado os exercícios

--- PythonExercicios/ex023.py
+++ PythonExercicios/ex023.py
@@ -1,3 +1,11 @@
-n = input('Digite um número: ')
+n = int(input('Digite um número: '))
 print('Analisando o número {}'.format(n))
+print('Unidade: {}'.format(n % 10))
+print('Dezena: {}'.format(n % 100 // 10))
+print('Centena: {}'.format(n % 1000 // 100))
+print('Milhar: {}'.format(n // 1000))
 
+u = n // 1 % 10
+d = n // 10 % 10
+c = n // 100 % 10
+m = n // 1000 % 10
--- PythonExercicios/ex026.py
+++ PythonExercicios/ex026.py
@@ -1,5 +1,5 @@
-frase = input('Digite uma frase: ')
+frase = str(input('Digite uma frase: ')).strip()
 total = frase.lower().count('a')
-first = frase.lower().find('a')
-last = frase.index('v', 0, len(frase))
+first = frase.lower().find('a') + 1
+last = frase.lower().rfind('a') + 1
 print('A letra "a" aparece {} vezes, a primeira em {} e a última em {}'.format(total, first, last))
