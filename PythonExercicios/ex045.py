08d629ff096deae68941d1939789d50d22031f7a
Fabricio Lelis <fabriciolelis@gmail.com>
2018-02-20 19:21:14 -0300

    Exercicios finalizados com condições aninhadas

--- PythonExercicios/ex045.py
+++ PythonExercicios/ex045.py
@@ -5,25 +5,24 @@ print('Suas opções:\n'
       '[ 0 ] PEDRA\n'
       '[ 1 ] PAPEL\n'
       '[ 2 ] TESOURA')
-opcao = int(input('Qual a sua jogada? '))
-sua_escolha = ''
-comp_escolha = ''
+jogador_opcao = int(input('Qual a sua jogada? '))
+jogador_escolha = ''
+computador_escolha = ''
 
-if opcao == 0:
-    sua_escolha = 'Pedra'
-elif opcao == 1:
-    sua_escolha = 'Papel'
-elif opcao == 2:
-    sua_escolha = 'Tesoura'
+if jogador_opcao == 0:
+    jogador_escolha = 'Pedra'
+elif jogador_opcao == 1:
+    jogador_escolha = 'Papel'
+elif jogador_opcao == 2:
+    jogador_escolha = 'Tesoura'
 
-comp_opcao = randint(0, 2)
-print('Computador opção', comp_opcao)
-if comp_opcao == 0:
-    comp_escolha = 'Pedra'
-elif comp_opcao == 1:
-    comp_escolha = 'Papel'
-elif comp_opcao == 2:
-    comp_escolha = 'Tesoura'
+computador_opcao = randint(0, 2)
+if computador_opcao == 0:
+    computador_escolha = 'Pedra'
+elif computador_opcao == 1:
+    computador_escolha = 'Papel'
+elif computador_opcao == 2:
+    computador_escolha = 'Tesoura'
 
 print('JO')
 sleep(0.4)
@@ -34,5 +33,11 @@ sleep(0.4)
 
 print('-=-' * 10)
 print('Computador jogou {}\n'
-      'Jogador jogou {}'.format(comp_escolha, sua_escolha))
+      'Jogador jogou {}'.format(computador_escolha, jogador_escolha))
 print('-=-' * 10)
+if computador_opcao == jogador_opcao:
+    print('EMPATE')
+elif (jogador_opcao == 0 and computador_opcao == 2) or jogador_opcao > computador_opcao:
+    print('Você Venceu!!!')
+elif (computador_opcao == 0 and jogador_opcao == 2) or computador_opcao > jogador_opcao:
+    print('Computador Venceu!!!')
