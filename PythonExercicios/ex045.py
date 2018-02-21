78883e46c5da5fed09af8bef0fe7c8ef57bc10b3
Fabricio Melo <fabricio.melo@virtus.ufcg.edu.br>
2018-02-21 09:52:32 -0300

    Refactor JOKENPO game

--- PythonExercicios/ex045.py
+++ PythonExercicios/ex045.py
@@ -1,28 +1,14 @@
 from random import randint
 from time import sleep
 
+itens = ('Pedra', 'Papel', 'Tesoura')
 print('Suas opções:\n'
       '[ 0 ] PEDRA\n'
       '[ 1 ] PAPEL\n'
       '[ 2 ] TESOURA')
+
 jogador_opcao = int(input('Qual a sua jogada? '))
-jogador_escolha = ''
-computador_escolha = ''
-
-if jogador_opcao == 0:
-    jogador_escolha = 'Pedra'
-elif jogador_opcao == 1:
-    jogador_escolha = 'Papel'
-elif jogador_opcao == 2:
-    jogador_escolha = 'Tesoura'
-
 computador_opcao = randint(0, 2)
-if computador_opcao == 0:
-    computador_escolha = 'Pedra'
-elif computador_opcao == 1:
-    computador_escolha = 'Papel'
-elif computador_opcao == 2:
-    computador_escolha = 'Tesoura'
 
 print('JO')
 sleep(0.4)
@@ -33,7 +19,7 @@ sleep(0.4)
 
 print('-=-' * 10)
 print('Computador jogou {}\n'
-      'Jogador jogou {}'.format(computador_escolha, jogador_escolha))
+      'Jogador jogou {}'.format(itens[computador_opcao], itens[jogador_opcao]))
 print('-=-' * 10)
 if computador_opcao == jogador_opcao:
     print('EMPATE')
