0ce9fe78abc1beee0ba99691078f4645765511d1
Fabricio Melo <fabricio.melo@virtus.ufcg.edu.br>
2018-03-05 16:36:18 -0300

    Terminado exercicio 59

--- PythonExercicios/ex059.py
+++ PythonExercicios/ex059.py
@@ -8,3 +8,17 @@ while opcao != 5:
           '[ 4 ] novos números\n'
           '[ 5 ] sair do programa')
     opcao = int(input('Qual sua opção? '))
+    if opcao == 1:
+        print('A soma entre {} e {} é {}\n'.format(num1, num2, num1 + num2))
+    elif opcao == 2:
+        print('O produto entre {} e {} é {}\n'.format(num1, num2, num1 * num2))
+    elif opcao == 3:
+        if num1 > num2:
+            maior = num1
+        else:
+            maior = num2
+        print('O maior número entre {} e {} é {}\n'.format(num1, num2, maior))
+    elif opcao == 4:
+        num1 = int(input('Primeiro Valor: '))
+        num2 = int(input('Segundo Valor: '))
+print('Obrigado por usar o programa!')
