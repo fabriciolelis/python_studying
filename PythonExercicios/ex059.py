4792e71df730be135cc868935fa533e80d1c2f2a
Fabricio Melo <fabricio.melo@virtus.ufcg.edu.br>
2018-03-05 16:02:18 -0300

    Comecei o exercicio 59

---
+++ PythonExercicios/ex059.py
@@ -0,0 +1,10 @@
+num1 = int(input('Primeiro Valor: '))
+num2 = int(input('Segundo Valor: '))
+opcao = 0
+while opcao != 5:
+    print('[ 1 ] somar\n'
+          '[ 2 ] multiplicar\n'
+          '[ 3 ] maior\n'
+          '[ 4 ] novos números\n'
+          '[ 5 ] sair do programa')
+    opcao = int(input('Qual sua opção? '))
